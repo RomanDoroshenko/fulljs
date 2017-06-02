6495b7392e2fa814068f657f0a5bdb5657db2cf2
Roman_D <roman.doroshenko.tlt@gmail.com>
2017-06-02 17:01:45 +0400

    Added data API

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ import http from 'http';
 import express from 'express';
 import reload from 'reload';
 import apiRouter from './api/index';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
 
 
 const app = express();
 const server = http.createServer(app);
+
+app.use(sassMiddleware({
+  src: path.join(__dirname,'sass'),
+  dest: path.join(__dirname,'public')
+}));
 
 app.set('view engine','jade');
 app.set('port', process.env.PORT || 3000);
